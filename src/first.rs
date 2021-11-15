b2af539416a444e2dea801c124615faab774c3fb
antondc <hello@antoniodiaz.me>
2021-11-15 10:18:38 +0300

    Pop

--- src/first.rs
+++ src/first.rs
@@ -28,12 +28,26 @@ impl List {
   }
 
   pub fn push(&mut self, elem: i32) {
-    // Use of mem::replace to replace self.head temporarily with Link::Empty before replacing it with the new head of the list.
+    // Use of mem::replace to replace self.head temporarily with Link::Empty before replacing it again with the new head of the list.
+    // 1. [ Link::More<Foo> ], ( "Foo", Link::Empty )
+    // 2. [ Link::More<Bar> ], ( "Bar", Link::More<Foo> ), ( "Foo", Link::Empty )
     let new_node = Box::new(Node {
       elem: elem,
       next: mem::replace(&mut self.head, Link::Empty),
     });
 
     self.head = Link::More(new_node);
+  }
+  // We empty head first returning it with `mem::replace`, and match against returned unallocated head
+  // 1. [ Link::More<Bar> ], ( "Bar", Link::More<Foo> ), ( "Foo", Link::Empty )
+  // 2. [ Link::More<Foo> ], ( "Foo", Link::Empty )
+  pub fn pop(&mut self) -> Option<i32> {
+    match mem::replace(&mut self.head, Link::Empty) {
+      Link::Empty => None,
+      Link::More(node) => {
+        self.head = node.next;
+        Some(node.elem)
+      }
+    }
   }
 }
