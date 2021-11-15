edb677c84b8c0207a7f0bcd6c9af42c589254ff8
antondc <hello@antoniodiaz.me>
2021-11-15 09:23:41 +0300

    Separate List and Link

--- src/first.rs
+++ src/first.rs
@@ -1,13 +1,18 @@
 // -Tail of a list never allocates extra junk
 // enum is in null-pointer-optimized form
 // All elements are uniformly allocated
+// Split List and Link to hide implementation details
+
+pub struct List {
+  head: Link,
+}
+
+enum Link {
+  Empty,
+  More(Box<Node>),
+}
 
 struct Node {
   elem: i32,
-  next: List,
-}
-
-pub enum List {
-  Empty,
-  More(Box<Node>),
+  next: Link,
 }
