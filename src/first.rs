749eb905919e5fe8b4dc2635f02def2346ecbaf6
antondc <hello@antoniodiaz.me>
2021-11-15 09:21:07 +0300

    Separate Node from List

--- src/first.rs
+++ src/first.rs
@@ -1,4 +1,13 @@
+// -Tail of a list never allocates extra junk
+// enum is in null-pointer-optimized form
+// All elements are uniformly allocated
+
+struct Node {
+  elem: i32,
+  next: List,
+}
+
 pub enum List {
   Empty,
-  Elem(i32, Box<List>),
+  More(Box<Node>),
 }
